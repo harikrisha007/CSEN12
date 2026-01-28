bfba3f3e79821728ab3b79ab2951440b3ccfea20
harikrisha007 <hkonala@student.gitam.edu>
2026-01-28 11:19:07 +0530

    Create 06_number_+_-_0.py

---
+++ 06_number_+_-_0.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive")
+elif num < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+#output
+Enter a number: 5
+The number is positive
