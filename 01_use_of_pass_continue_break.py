2bf9fd27f0c0d31111fb00ed6702b8930a35e4ce
harikrisha007 <hkonala@student.gitam.edu>
2026-01-28 10:30:59 +0530

    Create 01_use_of_pass_continue_break.py

---
+++ 01_use_of_pass_continue_break.py
@@ -0,0 +1,25 @@
+for i in range(1, 100):
+    if i % 2 == 0:
+        continue  
+    elif i == 25:
+        break     
+    else:
+        pass     
+
+    print(i)
+
+
+#output
+
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
