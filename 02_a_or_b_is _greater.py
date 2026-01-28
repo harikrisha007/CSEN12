9b780e19bb6895960a6089c9d0ac60528d8ae5ad
harikrisha007 <hkonala@student.gitam.edu>
2026-01-28 10:46:57 +0530

    Create 02_a_or_b_is _greater.py

---
+++ 02_a_or_b_is _greater.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+
+if a > b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+
+#output
+enter a number55
+enter a number44
+a is greater
