13c977225419ab6c907bf2fd6ebbf49182df6bdc
harikrisha007 <hkonala@student.gitam.edu>
2026-01-28 11:26:39 +0530

    Create 08_exit_program.py

---
+++ 08_exit_program.py
@@ -0,0 +1,15 @@
+print("Welcome to the program")
+
+choice = input("Do you want to continue? (yes/no): ")
+
+if choice.lower() == "no":
+    print("Exiting the program...")
+    exit()   # exits the program
+
+print("You chose to continue!")
+
+
+#output
+Welcome to the program
+Do you want to continue? (yes/no): no
+Exiting the program...
