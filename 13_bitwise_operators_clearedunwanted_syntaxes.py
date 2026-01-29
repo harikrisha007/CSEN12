5a8a45419b3e0b465edbc1836a1b42132e36e77f
harikrisha007 <hkonala@student.gitam.edu>
2026-01-29 15:23:16 +0530

    Create 13_bitwise_operators_clearedunwanted_syntaxes.py

---
+++ 13_bitwise_operators_clearedunwanted_syntaxes.py
@@ -0,0 +1,17 @@
+a=55
+b=789
+print("Bitwise Operators")
+print(f"{a} & {b} = {a & b}")
+print(f"{a} | {b} = {a | b}")
+print(f"Bitwise XOR {a} ^ {b} = {a ^ b}")
+print(f"Left Shift {a} << 2 = {a << 2}")
+print(f"Right Shift {a} >> 2 = {a >> 2}")
+
+
+#output
+Bitwise Operators
+55 & 789 = 21
+55 | 789 = 823
+Bitwise XOR 55 ^ 789 = 802
+Left Shift 55 << 2 = 220
+Right Shift 55 >> 2 = 13
