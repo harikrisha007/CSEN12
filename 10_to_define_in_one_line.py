d781f657194b4719347a4c897f6f6c0b273ca5f6
harikrisha007 <hkonala@student.gitam.edu>
2026-01-29 14:54:56 +0530

    Create 10_to_define_in_one_line.py

---
+++ 10_to_define_in_one_line.py
@@ -0,0 +1,14 @@
+a = 55;b = 69
+print("Arithmetic Operators");print(f"{a} + {b} = {a + b}");
+print(f"{a} - {b} = {a - b}");print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b:.2f}");print(f"{a} % {b} = {a % b}\n")
+
+
+
+#output
+Arithmetic Operators
+55 + 69 = 124
+55 - 69 = -14
+55 * 69 = 3795
+55 / 69 = 0.80
+55 % 69 = 55
