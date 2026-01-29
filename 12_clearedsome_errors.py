d209c5e2429bebaa364a48a40fb4b9737a6ab1b3
harikrisha007 <hkonala@student.gitam.edu>
2026-01-29 15:18:51 +0530

    Create 12_clearedsome_errors.py

---
+++ 12_clearedsome_errors.py
@@ -0,0 +1,14 @@
+a = 10
+b = 0
+
+print("Logical Operators")
+print(f"{a} and {b} = {a and b}")
+print(f"{a} or {b} = {a or b}")
+print(f"not {a} = {not a}\n")
+
+
+#output
+Logical Operators
+10 and 0 = 0
+10 or 0 = 10
+not 10 = False
