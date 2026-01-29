13e7e354634a7f40a6625f82a046e9fd34738b7f
harikrisha007 <hkonala@student.gitam.edu>
2026-01-29 15:06:14 +0530

    Create 11_gave_inputs_for_the_code.py

---
+++ 11_gave_inputs_for_the_code.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("Relational Operators")
+print(f"{a} < {b} = {a < b}")
+print(f"{a} > {b} = {a > b}")
+print(f"{a} == {b} = {a == b}")
+print(f"{a} != {b} = {a != b}\n")
+
+
+#output
+Enter a: 55
+Enter b: 69
+Relational Operators
+55 < 69 = True
+55 > 69 = False
+55 == 69 = False
+55 != 69 = True
