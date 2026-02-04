22ea917c20cd16284ebda35f3614a4acd3510544
harikrisha007 <hkonala@student.gitam.edu>
2026-02-04 11:06:10 +0530

    Create Importing Package + String.py

---
+++ Importing Package + String.py
@@ -0,0 +1,15 @@
+import math
+
+word = "Hello"
+length = len(word)
+
+print("Word:", word)
+print("Length:", length)
+print("Square root of length:", math.sqrt(length))
+
+
+
+#output
+Word: Hello
+Length: 5
+Square root of length: 2.2360679
