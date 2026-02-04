bebe4cade7da1430f60695bb695c833e79869a30
harikrisha007 <hkonala@student.gitam.edu>
2026-02-04 11:19:52 +0530

    Create String Operations & Slicing.py

---
+++ String Operations & Slicing.py
@@ -0,0 +1,14 @@
+a = "Hello"
+b = "World"
+
+c = a + " " + b
+print("Combined:", c)
+
+print("Slice:", c[0:5])
+print("Repeated:", a * 2)
+
+
+#output
+Combined: Hello World
+Slice: Hello
+Repeated: HelloHello
