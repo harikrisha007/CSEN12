0f262bff39334f119b9c8b0fe0b266a4d58fdbf8
harikrisha007 <hkonala@student.gitam.edu>
2026-02-04 10:58:16 +0530

    Create In-built Functions + String Basics.py

---
+++ In-built Functions + String Basics.py
@@ -0,0 +1,12 @@
+text = "Python"
+
+print("Text:", text)
+print("Length:", len(text))
+print("Type:", type(text))
+print("First letter:", text[0])
+
+#output
+Text: Python
+Length: 6
+Type: <class 'str'>
+First letter: P
