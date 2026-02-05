d272cc677fc2a88a6ae0d4235ead9ea53ae105a8
harikrisha007 <hkonala@student.gitam.edu>
2026-02-05 14:31:03 +0530

    Create Structure and Elements of a Python Program.py

---
+++ Structure and Elements of a Python Program.py
@@ -0,0 +1,18 @@
+name = input("Enter student name: ")
+m1 = int(input("Enter marks 1: "))
+m2 = int(input("Enter marks 2: "))
+m3 = int(input("Enter marks 3: "))
+
+total = m1 + m2 + m3
+average = total / 3
+
+print("Name:", name)
+print("Total Marks:", total)
+print("Average:", average)
+
+
+
+#output
+Name: Ravi
+Total Marks: 240
+Average: 80.0
